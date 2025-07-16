b88c2864a59bfab92e9f8cf8ff5d0954c3a7584e
Sharvan kumar <sharvank1515@gmail.com>
2025-07-17 00:10:09 +0530

    First Commit

---
+++ basic-of-spring-boot/src/main/java/com/example/basic_of_spring_boot/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.basic_of_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Basic of Spring Boot!";
+    }
+}
